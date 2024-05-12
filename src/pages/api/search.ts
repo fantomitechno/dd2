801fb282d2d67b77d973566e37fc387e1f22b352
Simon - fantomitechno <simon@renoux.dev>
2024-05-12 02:53:51 +0200

    ✨ Search API to search for a user

---
+++ src/pages/api/search.ts
@@ -0,0 +1,32 @@
+import type { APIRoute } from "astro";
+import type { Leaderboard } from "../../types/api";
+import { getRoute } from "../../utils/cache";
+import { similarity } from "../../utils/similarity";
+
+const ROUTE = "https://dips-plus-plus.xk.io/leaderboard/global";
+
+export const GET: APIRoute = async ({ url }) => {
+  const username = url.searchParams.get("username");
+  if (!username) {
+    return new Response(JSON.stringify({ message: "No username specified" }), { status: 400 });
+  }
+
+  let lb: Leaderboard = await getRoute(ROUTE);
+
+  let result = lb.find(p => similarity(p.name, username) > 0.8);
+
+  let i = 1;
+  while (result == null && lb.find(p => p.height > 3) && i < 100) {
+    lb = await getRoute(ROUTE + `/${i}`);
+    result = lb.find(p => similarity(p.name, username) > 0.8);
+    i++;
+  }
+
+  if (!result) {
+    return new Response(JSON.stringify({
+      message: "User not found in the first 100 pages of the leaderboard"
+    }), { status: 404 });
+  }
+
+  return new Response(JSON.stringify(result), { status: 200 });
+}
