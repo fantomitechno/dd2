2e51a582a9d11ce9975beab7fe8bdb36044fb87b
Simon - fantomitechno <fantomitechno@gmail.com>
2024-05-20 23:10:27 +0200

    :bug: I can't do proper TS from web editor

--- src/pages/api/nightbot_search.ts
+++ src/pages/api/nightbot_search.ts
@@ -1,36 +1,7 @@
 import type { APIRoute } from "astro";
-import { getPlayer } from "../../utils/cache";
-import { getPlayerPb } from "../../utils/api";
 
 export const GET: APIRoute = async ({ url }) => {
   return new Response(JSON.stringify({
     message: "API was disabled",
   }), { status: 200 });
-  const username = url.searchParams.get("username");
-  if (!username) {
-    return new Response(JSON.stringify({
-      message: "No username specified",
-      error: true
-    }), { status: 200 });
-  }
-
-  if (username.length < 4) {
-    return new Response(JSON.stringify({
-      message: "Username should be longer than 4 characters",
-      error: true
-    }), { status: 200 })
-  }
-
-  const result = await getPlayer(username);
-
-  if (!result) {
-    return new Response(JSON.stringify({
-      message: `${username} never played the map, they should get up and give it a try...`,
-      error: true
-    }), { status: 200 });
-  }
-
-  const data = await getPlayerPb(result);
-
-  return new Response(JSON.stringify({ ...data, error: false }), { status: 200 });
 }
--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -1,33 +1,7 @@
 import type { APIRoute } from "astro";
-import { getPlayer } from "../../utils/cache";
-import { getPlayerPb } from "../../utils/api";
 
 export const GET: APIRoute = async ({ url }) => {
   return new Response(JSON.stringify({
     message: "API was disabled",
   }), { status: 500 });
-  const username = url.searchParams.get("username");
-  if (!username) {
-    return new Response(JSON.stringify({
-      message: "No username specified"
-    }), { status: 400 });
-  }
-
-  if (username.length < 4) {
-    return new Response(JSON.stringify({
-      message: "Username should be longer than 4 characters"
-    }), { status: 400 })
-  }
-
-  const result = await getPlayer(username);
-
-  if (!result) {
-    return new Response(JSON.stringify({
-      message: "User not found in the first 100 pages of the leaderboard"
-    }), { status: 404 });
-  }
-
-  const data = await getPlayerPb(result);
-
-  return new Response(JSON.stringify(data), { status: 200 });
 }
