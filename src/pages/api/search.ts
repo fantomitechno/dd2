062e8f95bc51fd431eff62b3ff3e43a9e3917468
Simon - fantomitechno <fantomitechno@gmail.com>
2024-05-20 23:19:36 +0200

    :clown_face: ........

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -1,6 +1,6 @@
 import type { APIRoute } from "astro";
 
-export const GET: APIRoute = async ({ url }) => {
+export const GET: APIRoute = async () => {
   return new Response(JSON.stringify({
     message: "API was disabled",
   }), { status: 500 });
