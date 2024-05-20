cf0c24397edaac29c96eff4f478f6374b286a69e
Simon - fantomitechno <fantomitechno@gmail.com>
2024-05-20 23:17:20 +0200

    :clown_face: Forgot about nightbot :D

--- src/pages/api/nightbot_search.ts
+++ src/pages/api/nightbot_search.ts
@@ -3,5 +3,6 @@ import type { APIRoute } from "astro";
 export const GET: APIRoute = async ({ url }) => {
   return new Response(JSON.stringify({
     message: "API was disabled",
+    error: true,
   }), { status: 200 });
 }
