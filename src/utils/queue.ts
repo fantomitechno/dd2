ed436b1a011284ba02ab1d2280004a15ab9d5545
Simon - fantomitechno <simon@renoux.dev>
2024-05-24 14:20:48 +0200

    🐛 Use a single interval

--- src/utils/queue.ts
+++ src/utils/queue.ts
@@ -3,34 +3,17 @@ class Queue<T> {
   readonly queue: T[];
   readonly time: number;
 
-  private working = false;
-  private intervale: NodeJS.Timeout | null = null;
-
   constructor(action: (el: T) => any, queue: T[] = [], time: number = 1000) {
     this.action = action;
     this.queue = queue;
     this.time = time;
+    setInterval(() => {
+      if (this.queue.length == 0) this.action(this.queue.shift()!);
+    }, this.time);
   }
 
   add = (element: T) => {
     this.queue.push(element);
-
-    if (!this.working) {
-      this.working = true;
-      this.intervale = setInterval(() => {
-        if (this.queue.length == 0) {
-          clearInterval(this.intervale!);
-          this.intervale = null;
-          this.working = false;
-        }
-        else this.action(this.queue.shift()!);
-      }, this.time);
-    };
-  }
-
-
-  public get running(): boolean {
-    return this.intervale != null;
   }
 }
 
