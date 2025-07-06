e5320081229a376053d01f63d8214a572e6ff51e
Simon - fantomitechno <me@fantomitechno.dev>
2025-07-06 20:06:04 +0200

    ✨ Sip Floors

--- src/utils/floor.ts
+++ src/utils/floor.ts
@@ -16,9 +16,34 @@ const floorHeights = {
   "14": 1480.0, // 14
   "15": 1584.0, // 15
   "16": 1688.0, // 16
-  "Fin": 1905.0  // 17 fin
+  Fin: 1905.0, // 17 fin
 };
 
+const customFloorHeights: { [map: string]: { [floor: string]: number } } = {
+  slip: {
+    "0": 26,
+    "1": 162,
+    "2": 284.971,
+    "3": 417.45,
+    "4": 533.442,
+    "5": 653.516,
+    "6": 781.763,
+    "7": 906.247,
+    "8": 1037.3,
+    "9": 1166,
+    "10": 1293.92,
+    "11": 1421.69,
+    "12": 1553.02,
+    "13": 1677.59,
+    "14": 1809.5,
+    "15": 1935.46,
+    "16": 2061.73,
+    "17": 2185.16,
+    "18": 2317.66,
+    "19": 2449.52,
+    FIN: 2709,
+  },
+};
 
 export const heightToFloor = (height: number) => {
   let floorRes = "-1";
@@ -28,4 +53,16 @@ export const heightToFloor = (height: number) => {
     }
   }
   return floorRes;
-}
+};
+
+export const heightToFloorCustom = (type: string, height: number) => {
+  let floorRes = "-1";
+  for (const [floor, floorHeight] of Object.entries(
+    customFloorHeights[type] || {}
+  )) {
+    if (height >= floorHeight) {
+      floorRes = floor;
+    }
+  }
+  return floorRes;
+};
